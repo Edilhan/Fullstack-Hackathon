d8a2a618b4000398bee8457671787a8bf1d02a81
Edilhan <edylhan@gmail.com>
2022-08-23 17:50:41 +0600

    heroku5

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1nxzwz2v7ct!_h-4d$afc6-@hbhz0k3scmr3n9evr@023)2psh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['autoshop-top.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
